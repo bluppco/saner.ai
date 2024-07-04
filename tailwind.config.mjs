75ac7baa1c16545acd745209fa4c7057ff6ac5aa
diksha <rajdiksha91@gmail.com>
2024-07-04 22:37:58 +0530

    feat- announcement banner

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,63 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
-	darkMode: ["class"],
-  	content: [
-
-    	"./pages/**/*.{js,jsx}",
-    	"./components/**/*.{js,jsx}",
-    	"./app/**/*.{js,jsx}",
-    	"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-
-  	],
-  	prefix: "",
-  	theme: {
-
-		container: {
-
-			center: true,
-      		padding: "2rem",
-      		screens: {
-        		"2xl": "1400px",
-      		},
-
-		},
-    	extend: {
-
-			keyframes: {
-
-				"accordion-down": {
-
-					from: { height: "0" },
-          			to: { height: "var(--radix-accordion-content-height)" },
-
-				},
-        		"accordion-up": {
-
-					from: { height: "var(--radix-accordion-content-height)" },
-          			to: { height: "0" },
-
-				},
-      		},
-      		animation: {
-
-				"accordion-down": "accordion-down 0.2s ease-out",
-        		"accordion-up": "accordion-up 0.2s ease-out",
-
-			},
-			colors: {
-
-
-			},
-			fontFamily: {
-
-				"poppins": [ "Poppins", "sans-serif" ]
-
-			}
-
-		}
-
-	},
-  	plugins: [require("tailwindcss-animate")],
-
-}
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
+        color: {
+          primary: "#343C57",
+          secondary: "#FE890F",
+          tertiary: "#00FF00",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
