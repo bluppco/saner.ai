1f5f2cb75393da9802aaac5085713f24eae45cfb
Nandini <nandiniverma202021@gmail.com>
2024-07-13 19:48:31 +0530

    refactor:  tailwind.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -46,12 +46,12 @@ module.exports = {
 
 			},
 			colors: {
-				"text-neutral-400",
-				"text-gray-300",
-				"bg-gray-800",
-				"hover:bg-teal-300",
-				"text-white",
-				"hover:text-black",
+				"text-neutral-400" : "",
+				"text-gray-300" : "",
+				"bg-gray-800" : "",
+				"hover:bg-teal-300" : "",
+				"text-white" : "",
+				"hover:text-black": "",
 
 			},
 			fontFamily: {
